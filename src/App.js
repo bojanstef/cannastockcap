95f3d5810f09f4a7e953ae710a7df37895234fa3
Bojan Stefanovic <bojanstef4@gmail.com>
2018-05-28 19:52:10 -0700

    Initial commit with simple navbar only

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                {/* Create a <Toolbar /> element. */}
+                {/* Todo */}
+
+                {/* Create a <Navbar /> element. */}
+                <nav className="navbar navbar-expand-md navbar-light bg-light">
+                    <a className="navbar-brand" href="#">CannaStockCap</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsible-navbar" aria-controls="collapsible-navbar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsible-navbar">
+                        <ul className="navbar-nav mr-auto"></ul>
+                        <form className="form-inline my-2 my-md-0">
+                        <input className="form-control" type="text" placeholder="Search" />
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default App;
