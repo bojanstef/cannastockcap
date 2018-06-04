969849c47a4255f64c9abea6996161c5992cfc41
Bojan Stefanovic <bojan@formathletica.com>
2018-06-04 11:52:06 -0700

    Updated navbar to not collapse

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,17 +9,13 @@ export class Navbar extends Component {
                     <a className="navbar-brand" href="/">
                         <img src="/logo.svg" width="320" height="80" alt="Logo" />
                     </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsible-navbar" aria-controls="collapsible-navbar" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="collapsible-navbar">
-                        <ul className="navbar-nav mr-auto"></ul>
+                    <div>
                         <form className="form-inline my-2 my-md-0">
                         <div className="input-group">
-                        <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-companies" />
-                        <div className="input-group-append">
-                            <span className="input-group-text" id="search-companies"><i className="fa fa-search"></i></span>
-                        </div>
+                            <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-companies" />
+                            <div className="input-group-append">
+                                <span className="input-group-text" id="search-companies"><i className="fa fa-search"></i></span>
+                            </div>
                         </div>
                         </form>
                     </div>
