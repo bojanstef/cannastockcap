3e07f519b71be41acd249a9e6bef416c22a94b6b
Bojan Stefanovic <bojan@formathletica.com>
2018-06-07 12:14:22 -0700

    Added google analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'font-awesome/css/font-awesome.min.css'; 
+
+ReactGA.initialize('UA-120527938-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <Router>
